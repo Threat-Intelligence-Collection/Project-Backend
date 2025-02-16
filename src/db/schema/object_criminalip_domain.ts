7457bf8d09d6a3233835797fcce12ba4a7b23297
Kittiwin Phannachet <kittiwinktb@gmail.com>
2025-02-16 13:43:26 +0700

    update database

--- src/db/schema/object_criminalip_domain.ts
+++ src/db/schema/object_criminalip_domain.ts
@@ -1,9 +1,11 @@
-import { pgTable, integer } from "drizzle-orm/pg-core";
+import { pgTable, integer, text } from "drizzle-orm/pg-core";
 import { objects } from "./objects";
 
 export const object_criminalip_domain = pgTable("object_criminalip_domain", {
   id: integer("id")
-    .primaryKey().generatedAlwaysAsIdentity()
+    .primaryKey()
+    .generatedAlwaysAsIdentity()
     .references(() => objects.id, { onDelete: "cascade" }),
   score: integer("score"),
+  issue: text("issue").array(), 
 });
