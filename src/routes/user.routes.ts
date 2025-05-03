02b9d6a54f102ea15263c431a774b40a0497c4bd
Kittiwin Phannachet <110970175+KP2301@users.noreply.github.com>
2025-05-03 11:46:09 +0700

    merge Analyze score to main2 (#30)
    * making model for adding user not completed
    
    * Add searchAsset functionality and related types
    
    * make deleteUser and getUserByEmail
    
    * make updateUser and set logger to false in client.ts
    
    * define relation for database
    
    * add transaction to userService and adjust the enum for user_role
    
    * Enhance risk assessment features by adding calculateDomainRisk and calculateIPRisk functions, and update related response types to include risk scores.
    
    * Create SECURITY.md (#24)
    
    * add event model
    
    * IsMalicious down and Finishing Structure Analyzer Part
    
    * add attribute and object model
    
    * Implement GitHub and Google OAuth login functionality with JWT token creation and user data handling
    
    * Refactor GitHub and Google OAuth services to use environment variables for API URLs
    
    * doing tag table
    
    * Revert "Merge branch 'making-model' into analyze_score"
    
    This reverts commit d1b3fa46c515ed52f5df53a3f47b1a3b32797383, reversing
    changes made to 52bc4a96d5c1b542f9cb83db9fd93a2440b11742.
    
    * Reapply "Merge branch 'making-model' into analyze_score"
    
    This reverts commit 13c25a0e066b715d5a671518dc9777bae2b2f168.
    
    ---------
    
    Co-authored-by: GOLF2546 <104701283+GOLF2546@users.noreply.github.com>
    Co-authored-by: GOLF2546 <Pattarapolnanakorn@gmail.com>

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,14 +1,29 @@
 import { Elysia } from "elysia";
-import { searchDomain, searchIP } from "@src/controllers/searchController/search.controller";
+import { searchDomain, searchIP, searchAsset } from "@src/controllers/searchController/search.controller";
+import { GithubOAuthController } from "@src/controllers/loginController/githubLogIn.controller";
+import { GoogleAuthController } from "@src/controllers/loginController/googleLogin.controller";
 
 export const appRoutes = new Elysia()
   .get("/search/ip_address/:ip", searchIP, {
     detail: {
-      tags: ["Search"],
+      tags: ["Search_IP"],
     },
   })
   .get("/search/domain_name/:domainName", searchDomain, {
     detail: {
-      tags: ["Search"],
+      tags: ["Search_Domain"],
     },
+  })
+  .get("/search/asset/:asset", searchAsset, {
+    detail: {
+      tags: ["Search_Asset"]
+    }
+  }).get("/login/oauth2/code/github", GithubOAuthController, {
+    detail: {
+      tags: ["Github_OAuth"],
+    }
+  }).get("/login/oauth2/code/google", GoogleAuthController, {
+    detail: {
+      tags: ["Google_OAuth"],
+    }
   });
